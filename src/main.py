6d99d1589b8003e05d6bc7b6e170c0537910c42a
wmedrano <will.s.medrano@gmail.com>
2024-05-06 09:49:26 -0700

    Send response to channel messages.

--- src/main.py
+++ src/main.py
@@ -2,11 +2,20 @@ import discord
 import os
 
 class WenardClient(discord.Client):
-    async def on_ready(self):
-        pass
+    async def on_message(self, message: discord.Message):
+        channel = self.channel_for_response(message)
+        if channel is None:
+            return
+        await channel.send("You called?")
 
-    async def ono_message(self, message):
-        pass
+    def channel_for_response(self, message: discord.Message):
+        if message.author == self.user:
+            return None
+        if not any(user == self.user for user in message.mentions):
+            return None
+        if not isinstance(message.channel, discord.TextChannel):
+            return None
+        return message.channel
 
 def main():
     discord_token = os.environ['WENARD_TOKEN']
@@ -14,6 +23,7 @@ def main():
         raise KeyError('Environment variable WENARD_TOKEN not found. This must be set to a valid Discord bot token.')
 
     intents = discord.Intents.default()
+    intents.messages = True
     client = WenardClient(intents=intents)
     client.run(discord_token)
 
